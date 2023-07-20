57ac60a19940b8d1abd2945556fafbd614f92ac6
GANGADASARISAILAKSHMI <130964719+GANGADASARISAILAKSHMI@users.noreply.github.com>
2023-07-20 21:49:25 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package thread;
+
+public class Main extends Thread{
+	int count=0;
+	public void run()
+	{
+		for(int i=0;i<=777;i++)
+		{
+			count++;
+		}
+		System.out.println("Completed Thread: "+Thread.currentThread().getName());
+		System.out.println("Its Priority: "+Thread.currentThread().getPriority());
+		
+	}
+
+}
